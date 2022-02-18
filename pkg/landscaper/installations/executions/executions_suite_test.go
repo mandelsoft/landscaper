68d8e459f930106b947dbf25b216f3fb63af5f42
Johannes Aubart <johannes.aubart@sap.com>
2022-02-18 11:03:12 +0100

    fix defaulting and add tests for deployitem target import rendering

---
+++ pkg/landscaper/installations/executions/executions_suite_test.go
@@ -0,0 +1,35 @@
+// SPDX-FileCopyrightText: 2020 SAP SE or an SAP affiliate company and Gardener contributors.
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package executions_test
+
+import (
+	"context"
+	"os"
+	"path/filepath"
+	"testing"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+
+	componentsregistry "github.com/gardener/landscaper/pkg/landscaper/registry/components"
+
+	testutils "github.com/gardener/landscaper/test/utils"
+)
+
+func TestConfig(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Template Executions Test Suite")
+}
+
+func createDefaultContextsForNamespace(kubeClient client.Client) {
+	// create default repo for all namespaces
+	repoCtx := componentsregistry.NewLocalRepository("../testdata/registry")
+	list, err := os.ReadDir("./testdata/state")
+	Expect(err).To(Succeed())
+	for _, d := range list {
+		Expect(testutils.CreateDefaultContext(context.TODO(), kubeClient, repoCtx, filepath.Base(d.Name()))).To(Succeed())
+	}
+}
